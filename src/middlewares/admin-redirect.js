aa4aacffc49776181a5fba6d0247e810766202ce
Aigars Peda <a.peda@lvm.lv>
2024-06-06 15:16:08 +0300

    custom middleware to redirect from /

--- src/middlewares/admin-redirect.js
+++ src/middlewares/admin-redirect.js
@@ -1,10 +1,12 @@
 module.exports = (_config, { strapi }) => {
-  const redirects = ["/", "/index.html"].map((path) => ({
-    method: "GET",
-    path,
-    handler: (ctx) => ctx.redirect("/admin"),
-    config: { auth: false },
-  }));
+  const redirects = ["/", "/index.html"].map((path) => {
+    return {
+      method: "GET",
+      path,
+      handler: (ctx) => ctx.redirect("/strapi/admin/"),
+      config: { auth: false },
+    };
+  });
 
   strapi.server.routes(redirects);
 };
